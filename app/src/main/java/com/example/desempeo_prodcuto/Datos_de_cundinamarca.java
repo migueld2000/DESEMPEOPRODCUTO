ab94575b1dbf257600e9a9b5cfc3912021e7732b
migueld2000 <70757824+migueld2000@users.noreply.github.com>
2021-06-14 16:49:33 -0500

    desarrollo_menu_integrado

---
+++ app/src/main/java/com/example/desempeo_prodcuto/Datos_de_cundinamarca.java
@@ -0,0 +1,14 @@
+package com.example.desempeo_prodcuto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Datos_de_cundinamarca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos_de_cundinamarca);
+    }
+}
