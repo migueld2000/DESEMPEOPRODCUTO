fecad2015481e6085ccf352509e26fbd7e273441
migueld2000 <70757824+migueld2000@users.noreply.github.com>
2021-06-14 18:36:49 -0500

    desarrollo_menu_parte2

--- app/src/main/java/com/example/desempeo_prodcuto/Home.java
+++ app/src/main/java/com/example/desempeo_prodcuto/Home.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -26,6 +29,35 @@ public class Home extends AppCompatActivity
         ListaAdaptador adaptador=new ListaAdaptador(lista_targetas);
         listado_grafico.setAdapter(adaptador);
     }
+
+    //Metodo encargado de mostrar el menu
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id=item.getItemId();
+        switch (id)
+        {
+            case(R.id.opcion1):
+
+                Intent intent1=new Intent(Home.this,Datos_de_cundinamarca.class);
+                startActivity(intent1);
+
+                break;
+
+            case(R.id.opcion2):
+
+                Intent intent2=new Intent(Home.this,Economia.class);
+                startActivity(intent2);
+                break;
+        }
+        return  super.onOptionsItemSelected(item);
+    }
+
     private void crear_listado()
     {
         lista_targetas.add(new Cartas("Zipaquirá",getString(R.string.Descripcion1),R.drawable.zipaquira));
